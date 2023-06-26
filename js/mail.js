8a8e27cf80b1373c5e9f304bf8da2bb164768554
MaineKoon88 <89954728+MaineKoon88@users.noreply.github.com>
2023-06-26 00:36:27 -0300

    Add files via upload

--- js/mail.js
+++ js/mail.js
@@ -1,28 +1,62 @@
-
-    var btn = document.getElementById('btn');
-
-btn.addEventListener('click', function(e) {
-  e.preventDefault();
+document.getElementById('btn').addEventListener('click', function(event) {
+  event.preventDefault(); // Evitar el envío del formulario por defecto
+  
   var name = document.getElementById('Nombre').value;
   var email = document.getElementById('email').value;
   var subject = document.getElementById('Asunto').value;
   var message = document.getElementById('Mensaje').value;
-
-  if (name === '' || email === '' || subject === '' || message === '') {
-    alert('Por favor, complete todos los campos.');
-  } else {
+  
+  var inputs = document.getElementsByClassName('input100');
+  var isValid = true;
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  Array.prototype.forEach.call(inputs, function(input) {
+    if (input.name === 'email') {
+      if (input.value.match(emailRegex)) {
+        input.parentNode.classList.remove('alert-validate');
+      } else {
+        input.parentNode.classList.add('alert-validate');
+        isValid = false;
+      }
+    } else {
+      if (input.checkValidity()) {
+        input.parentNode.classList.remove('alert-validate');
+      } else {
+        input.parentNode.classList.add('alert-validate');
+        isValid = false;
+      }
+    }
+  });
+  
+  var errorMessage = document.getElementById('errorMessage');
+  if (isValid && email.match(emailRegex)) {
     var body = 'Nombre: ' + name + '<br/> email: ' + email + '<br/> Asunto: ' + subject + '<br/> Mensaje: ' + message;
 
     Email.send({
       SecureToken: "2fd5a4c5-0e3f-4e3f-aabc-e7953f57cff5",
       To: 'nacho.reynoso@yahoo.com.ar',
       From: "nacho.pgnp@gmail.com",
-      Subject: "mensaje de contacto",
+      Subject: "Mensaje de contacto",
       Body: body
     }).then(function(message) {
-      alert('Mensaje Enviado!');
+      var btn = document.getElementById("btn");
+      btn.innerHTML = "Mensaje Enviado!";
       resetForm();
+      setTimeout(function() {
+        btn.innerHTML = '<span>Enviar <i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i></span>';
+      }, 3000);
     });
+  } else {
+    errorMessage.textContent = 'Complete el formulario';
+    
+    
+    var container = document.getElementsByClassName('container-contact100-form-btn')[0];
+    container.appendChild(errorMessage);
+
+    setTimeout(function() {
+      errorMessage.style.display = 'none';
+    }, 3000);
+  
   }
 });
 
@@ -31,4 +65,5 @@ function resetForm() {
   document.getElementById('email').value = '';
   document.getElementById('Asunto').value = '';
   document.getElementById('Mensaje').value = '';
+  document.getElementById('errorMessage').style.display = 'none';
 }
