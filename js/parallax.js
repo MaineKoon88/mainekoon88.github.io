d995e5be8d649b64f6379b6168f39fcd16d66609
MaineKoon88 <89954728+MaineKoon88@users.noreply.github.com>
2023-07-01 22:55:09 -0300

    Add files via upload
    Armado de la sección neuro, todavía falta completar

--- js/parallax.js
+++ js/parallax.js
@@ -1,14 +1,14 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-
-    if (entry.isIntersecting) {
-        entry.target.classList.add('show');      
-    } else {
-      entry.target.classList.remove('show');
-    }
-
+document.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
   });
-});
 
-const hiddenElements = document.querySelectorAll('.hidden, .hidden2, .hidden3');
-hiddenElements.forEach((el) => observer.observe(el));
+  const hiddenElements = document.querySelectorAll('.hidden, .hidden2, .aneu1, .aneu2, .container5c, .container5c1, .label, .animation');
+  hiddenElements.forEach((el) => observer.observe(el));
+});
