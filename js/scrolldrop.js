e56a7d0d8330a162314c0658af7b72450f05b155
MaineKoon88 <89954728+MaineKoon88@users.noreply.github.com>
2023-07-11 21:58:03 -0300

    Add files via upload

--- js/scrolldrop.js
+++ js/scrolldrop.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
-    $('.dropbtn').on('scroll', function(e) {
-      e.preventDefault();
-      $('.dropdown-content').toggleClass('show');
-    });
-  
-    $(document).on('scroll', function(e) {
-      if (
-        !$('.dropbtn').is(e.target) &&
-        !$('.dropbtn ion-icon').is(e.target) &&
-        !$('.dropdown-content').is(e.target) &&
-        $('.dropdown-content').has(e.target).length === 0
-      ) {
-        $('.dropdown-content').removeClass('show');
-      }
-    });
-  
-    $(window).on('scroll', function() {
-      if ($(window).width() <= 990) {
-        $('.dropdown-content').removeClass('show');
-      }
-    });
+  $('.dropbtn').on('scroll', function(e) {
+    e.preventDefault();
+    $('.dropdown-content').toggleClass('show');
   });
-  
+
+  $(document).on('scroll', function(e) {
+    if (
+      !$('.dropbtn').is(e.target) &&
+      !$('.dropbtn ion-icon').is(e.target) &&
+      !$('.dropdown-content').is(e.target) &&
+      $('.dropdown-content').has(e.target).length === 0
+    ) {
+      $('.dropdown-content').removeClass('show');
+    }
+  });
+
+  $(window).on('scroll', function() {
+    if ($(window).width() <= 990) {
+      $('.dropdown-content').removeClass('show');
+    }
+  });
+});
